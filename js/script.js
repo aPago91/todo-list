9cd20b12ae09c889b6b224cebce5658b73672789
aPago91 <apagowski1991@gmail.com>
2018-08-14 11:13:42 +0200

    Dodanie do JS dodawanie zadania / przy braku tekstu nie dodaje zadanie

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 document.getElementById("add").addEventListener("click", function () {
 	var value = document.getElementById('item').value;
+	var list = document.getElementById('todo');
+	var input = document.querySelector('input');
 	if (value) {
 		console.log('Dodano nowe zadanie!');
+		var myItem = input.value;
+		input.value = '';
+		        
+		var listItem = document.createElement('li');
+		var listText = document.createElement('span');
+		
+		listItem.appendChild(listText);
+		listText.textContent = myItem;
+		list.appendChild(listItem);
+    
+		
+		
 	} else {
 		console.log('Brak tekstu');
 	}
