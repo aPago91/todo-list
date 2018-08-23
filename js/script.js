abb00ca1495e11bbc8978963adad665ecf859fa6
aPago91 <apagowski1991@gmail.com>
2018-08-23 22:50:54 +0200

    JS poprawka

--- js/script.js
+++ js/script.js
@@ -1,59 +1,53 @@
- document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM fully loaded and parsed");
-document.getElementById("add").addEventListener("click", function () {
-	var value = document.getElementById('item').value;
-	var list = document.getElementById('todo');
-	var input = document.querySelector('input');
-	if (value) {
-		console.log('Dodano nowe zadanie!');
-		var myItem = input.value;
-		input.value = '';
+document.addEventListener("DOMContentLoaded", function(event) {
+	console.log("DOM fully loaded and parsed");
+	document.getElementById("add").addEventListener("click", function () {
+		var value = document.getElementById('item').value;
+		var list = document.getElementById('todo');
+		var input = document.querySelector('input');
+		if (value) {
+			console.log('Dodano nowe zadanie!');
+			var myItem = input.value;
+			input.value = '';
 		        
-		var listItem = document.createElement('li');
-		var listText = document.createElement('span');
-		var listDiv = document.createElement ('div');
-		listDiv.setAttribute("id","btn")
-		var btnEdit = document.createElement("button");
-		btnEdit.setAttribute("class","Edit")
-		var btnDelete = document.createElement("button");
-		btnDelete.setAttribute("class","Delete")
-		var btnDone = document.createElement("button");
-		btnDone.setAttribute("class","Done")
+			var listItem = document.createElement('li');
+			var listText = document.createElement('span');
+			var listDiv = document.createElement ('div');
+			listDiv.setAttribute("id","btn");
+			var btnEdit = document.createElement("button");
+			btnEdit.setAttribute("class","Edit");
+			var btnDelete = document.createElement("button");
+			btnDelete.setAttribute("class","Delete");
+			var btnDone = document.createElement("button");
+			btnDone.setAttribute("class","Done");
 		
-		listItem.appendChild(listText);
-		listText.textContent = myItem;
-		listText.appendChild(listDiv);
-		listDiv.appendChild(btnEdit);
-		btnEdit.textContent = 'Edit';
-		listDiv.appendChild(btnDelete);
-		btnDelete.textContent = 'Delete';
-		listDiv.appendChild(btnDone);
-		btnDone.textContent = 'Done';
-		list.appendChild(listItem);
+			listItem.appendChild(listText);
+			listText.textContent = myItem;
+			listText.appendChild(listDiv);
+			listDiv.appendChild(btnEdit).textContent = 'Edit';
+			listDiv.appendChild(btnDelete).textContent = 'Delete';
+			listDiv.appendChild(btnDone).textContent = 'Done';
+			list.appendChild(listItem);
 		
-		var select = document.querySelector('todo');
-		select.addEventListener('change', setButton);
+			listItem.addEventListener('click', setButton);
+		
+			function setButton(event) {
+				var choice = event.target.className;
 
-		function setButton(event) {
-			var choice = event.target.className;
-
-			switch (choice) {
-				case 'Edit':
-					function myFunction() {
-					listText.contentEditable = true;
-					}
-					break;
-				case 'delete':
-						
-					break;
-				case 'done':
-					break;
-				default:
-			}
-		} 
-	}
+				switch (choice) {
+					case 'Edit':
+						document.getElementsByTagName("li")[0];
+						listText.contentEditable = true;
+						break;
+					case 'Delete':
+						break;
+					case 'Done':
+						break;
+					default:
+				}
+			} 
+		}
 		else {
 			console.log('Brak tekstu');
 		}
-})
- });
+	})
+});
